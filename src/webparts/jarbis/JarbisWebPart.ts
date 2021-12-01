130468f1190acf0a2803fb8c505cefea1863891e
Chris Kent <8364109+thechriskent@users.noreply.github.com>
2021-12-01 16:29:36 -0500

    Empty_Jarbis Baseline

--- src/webparts/jarbis/JarbisWebPart.ts
+++ src/webparts/jarbis/JarbisWebPart.ts
@@ -18,18 +18,7 @@ export default class JarbisWebPart extends BaseClientSideWebPart<IJarbisWebPartP
   public render(): void {
     this.domElement.innerHTML = `
       <div class="${ styles.jarbis }">
-        <div class="${ styles.container }">
-          <div class="${ styles.row }">
-            <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint!</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+        Wowee!!
       </div>`;
   }
 
